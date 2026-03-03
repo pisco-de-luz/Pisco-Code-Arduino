448f1bc103264ae1d12f31716b90785012c5fc7c
Andre Viegas <46757321+andreviegas@users.noreply.github.com>
2026-03-03 08:21:06 +0100

    Refactor header guard to safely undefine conflicting preprocessor macros in Pisco-Code library

--- src/Pisco-Code.h
+++ src/Pisco-Code.h
@@ -3,6 +3,23 @@
 #ifndef PISCO_CODE_ARDUINO_H
 #define PISCO_CODE_ARDUINO_H
 
+// Arduino's Print.h defines BIN, OCT, DEC, HEX as preprocessor macros
+// (e.g. #define BIN 2). These clash with the pisco_code::NumberBase enum
+// members of the same name.  Undefine them before pulling in the library;
+// the scoped enum (NumberBase::BIN, etc.) replaces them safely.
+#ifdef BIN
+#undef BIN
+#endif
+#ifdef OCT
+#undef OCT
+#endif
+#ifdef DEC
+#undef DEC
+#endif
+#ifdef HEX
+#undef HEX
+#endif
+
 #include "pisco_code.hpp"
 
 #endif // PISCO_CODE_ARDUINO_H
